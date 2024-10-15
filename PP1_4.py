aaf1ec3c89efd4fefc66e249d7e0112a9c504a6c
Dinara-Pathiraja-ICD2O1-02 <Dinara.pathiraja@student.tdsb.on.ca>
2024-10-15 18:51:58 +0000

    d

--- PP1_4.py
+++ PP1_4.py
@@ -1,21 +1,33 @@
 
 
+'''
+Lesson: Input and F strings
+Author: Dinara 
+Date created: Sept 19,2024
+Date last modified: Sept 19, 2024
+'''
 def q1():
-  #Write Assignment code here
-
+  answer = input ("Input a word: ")
+  print (answer)
 def q2():
-  #Write Assignment code here
+  name = input ("Input your first name: ")
+  print ("Hello " + name)
 
 def q3():
-  #Write Assignment code here
+  firstname = input ("Input your first name: ")
+  lastname = input ("Input your last name: ")
+  print(f"{lastname} {firstname}")
+
 
 def q4():
-  #Write Assignment code here
-
+  student1 = input ("Input a student: ")
+  student2 = input ("Input another student: ")
+  print (f"Your students are {student1} and {student2}")
+  
 #Comment Out the go below when you run your test cases.
 #Do not comment it out when you want to run your code normally
 
-q1()
-q2()
-q3()
-q4()
+# q1()
+# q2()
+# q3()
+# q4()
